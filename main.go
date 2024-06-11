4ec87611bdd11ea9ed93b809a65781ebefb39125
T_bag <111@111.com>
2024-06-11 14:50:50 +0800

    fix

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	//v1 "go-go-go/v1"
 	"go-go-go/database"
 	"go-go-go/router"
+	"fmt"
+	"time"
 	)
 
 
@@ -19,8 +21,24 @@ func main() {
 	database.DB.AutoMigrate(&models.Product{},&models2.VideoInfo{})
 
 	r := gin.Default()
+	r.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+
+		// 你的自定义格式
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
+	//r.Use(gin.Recovery())
 	router.InitRouter(r)
-	r.Run()
+	r.Run(":9000")
 
 }
 
