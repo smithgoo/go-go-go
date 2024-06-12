c59e914c1e116be44e4fb9196303a995d4b8f3a5
smithgoo <294336370@qq.com>
2024-06-12 17:54:41 +0800

    token 过期

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,10 +1,11 @@
 package middleware
 
 import (
-	"go-go-go/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go-go-go/utils" // 导入你的 utils 包，确保路径正确
+	"github.com/dgrijalva/jwt-go"
 )
 
 func AuthMiddleware() gin.HandlerFunc {
@@ -15,13 +16,26 @@ func AuthMiddleware() gin.HandlerFunc {
 			c.Abort()
 			return
 		}
+
 		claims, err := utils.ValidateJWT(tokenStr)
 		if err != nil {
+			// 如果 token 过期，返回相应的错误信息
+			if ve, ok := err.(*jwt.ValidationError); ok {
+				if ve.Errors&jwt.ValidationErrorExpired != 0 {
+					c.JSON(http.StatusUnauthorized, gin.H{"error": "token expired"})
+					c.Abort()
+					return
+				}
+			}
+
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
 			c.Abort()
 			return
 		}
+
 		c.Set("userID", claims.UserID)
 		c.Next()
 	}
 }
+
+
